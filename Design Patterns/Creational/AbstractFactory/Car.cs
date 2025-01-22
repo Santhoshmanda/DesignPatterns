bd1f3835c126933760df946c81cd557b059a95a2
Santhosh <40848983+Santhoshmanda@users.noreply.github.com>
2025-01-22 14:43:47 +0530

    Update Car.cs

--- Design Patterns/Creational/AbstractFactory/Car.cs
+++ Design Patterns/Creational/AbstractFactory/Car.cs
@@ -1,6 +1,119 @@
 ﻿using System;
 namespace Design_Patterns.Creational.AbstarctFactory
+{	using System;
+namespace AbstractFactoryDesignPattern
 {
+    // Abstract Products
+    public interface IButton
+    {
+        void Click();
+    }
+
+    public interface ITextBox
+    {
+        void Write(string text);
+    }
+
+    // Concrete Products for Windows
+    public class WindowsButton : IButton
+    {
+        public void Click()
+        {
+            Console.WriteLine("Windows Button Clicked");
+        }
+    }
+
+    public class WindowsTextBox : ITextBox
+    {
+        public void Write(string text)
+        {
+            Console.WriteLine($"Text Written in Windows TextBox: {text}");
+        }
+    }
+
+    // Concrete Products for MacOS
+    public class MacOSButton : IButton
+    {
+        public void Click()
+        {
+            Console.WriteLine("MacOS Button Clicked");
+        }
+    }
+
+    public class MacOSTextBox : ITextBox
+    {
+        public void Write(string text)
+        {
+            Console.WriteLine($"Text Written in MacOS TextBox: {text}");
+        }
+    }
+
+    // Abstract Factory
+    public interface IUIFactory
+    {
+        IButton CreateButton();
+        ITextBox CreateTextBox();
+    }
+
+    // Concrete Factory for Windows
+    public class WindowsUIFactory : IUIFactory
+    {
+        public IButton CreateButton() => new WindowsButton();
+
+        public ITextBox CreateTextBox() => new WindowsTextBox();
+    }
+
+    // Concrete Factory for MacOS
+    public class MacOSUIFactory : IUIFactory
+    {
+        public IButton CreateButton() => new MacOSButton();
+
+        public ITextBox CreateTextBox() => new MacOSTextBox();
+    }
+
+    // Client
+    public class Application
+    {
+        private readonly IButton _button;
+        private readonly ITextBox _textBox;
+
+        public Application(IUIFactory factory)
+        {
+            _button = factory.CreateButton();
+            _textBox = factory.CreateTextBox();
+        }
+
+        public void Render()
+        {
+            _button.Click();
+            _textBox.Write("Sample Text");
+        }
+    }
+    
+    // Testing the Abstract Factory Design Pattern
+    // Client Code
+    public class Client
+    {
+        public static void Main()
+        {
+            IUIFactory factory;
+
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                factory = new WindowsUIFactory();
+            }
+            else
+            {
+                factory = new MacOSUIFactory();
+            }
+
+            var app = new Application(factory);
+            app.Render();
+
+            Console.ReadKey();
+        }
+    }
+}
 	public interface Car
 	{
 		int GetTopSpeed();
